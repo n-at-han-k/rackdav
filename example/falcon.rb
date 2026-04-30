18c8582e0fb0a71f3106a11b127a7b73e30ab4a8
Nathan Kidd <nathankidd@hey.com>
2026-04-30 16:12:14 +0100

    'Quick Git Push'

--- example/falcon.rb
+++ example/falcon.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
 
-# Falcon configuration.
+# Falcon production configuration.
 #
 # Usage:
-#   cd example && bundle install && bundle exec falcon serve -b http://0.0.0.0:9292
+#   cd example && bundle exec falcon host
 #
-# Falcon will auto-detect config.ru in the current directory.
+# falcon host reads this file and starts the managed service.
+
+require "falcon/environment/rack"
+
+service "caldav" do
+  include Falcon::Environment::Rack
+
+  # The root directory for the application (where config.ru lives).
+  def root
+    __dir__
+  end
+
+  # Bind to all interfaces on port 9292 over plain HTTP.
+  # TLS termination is expected to happen at the reverse proxy layer.
+  def url
+    "http://0.0.0.0:9292"
+  end
+
+  # Number of worker processes. Defaults to Async::Container.processor_count.
+  # Uncomment to override:
+  # def count
+  #   4
+  # end
+end
